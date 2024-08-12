97fd6e8a588afda87473827a4a4f1f4a8a61745b
Micalli <bruno.micalli@uol.com.br>
2024-08-12 10:42:33 -0300

    remove console

--- fe/src/view/pages/Question/controllers/useFeedbackQuestionModalController.ts
+++ fe/src/view/pages/Question/controllers/useFeedbackQuestionModalController.ts
@@ -48,7 +48,6 @@ export function useFeedbackQuestionModalController() {
    };
 
   const rateAQuestion = async (data: RateQuestionParams) => {
-console.log('aq')
    await mutateQueryRateQuestion(data);
   };
 
